14e909c6348f83af5380f1ea4037909c86ac943d
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-24 21:48:09 -0300

    Add files via upload

---
+++ fintechMVP/main.java
@@ -0,0 +1,8 @@
+import org.catolicasc.bank.maruan.endereco;
+
+public class main {
+   public static void main(String args[]){
+    endereco endereco = new endereco();
+        System.out.println(endereco);
+    }
+}
