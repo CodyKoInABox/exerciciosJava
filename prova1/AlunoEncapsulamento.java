6c2a7af89e280bfc0f197fb39a98db340d9714eb
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-13 20:51:17 -0300

    prova1 codigo EXTRA

---
+++ prova1/AlunoEncapsulamento.java
@@ -0,0 +1,14 @@
+package prova1;
+
+public class AlunoEncapsulamento extends Pessoa{
+    private int matricula;
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+}
